7eb6b43a689df385bf16015943b0c81507e47cf0
vaibhavaiscoder <vaibhavghugase00@gmail.com>
2023-06-27 17:20:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyClass(),
+  ));
+}
+
+class MyClass extends StatefulWidget {
+  const MyClass({Key? key}) : super(key: key);
+
+  @override
+  _MyClassState createState() => _MyClassState();
+}
+
+class _MyClassState extends State<MyClass> {
+  var phone = "";
+  var msg = "";
+  var code = "+91";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("WhatsApp Direct"),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.only(left: 40, right: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: CountryCodePicker(
+                  onChanged: (val) {
+                    code = val.toString();
+                  },
+                  // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                  initialSelection: 'IN',
+                  favorite: ['+91', 'IN'],
+                  // optional. Shows only country name and flag
+                  showCountryOnly: false,
+                  // optional. Shows only country name and flag when popup is closed.
+                  showOnlyCountryWhenClosed: false,
+                  // optional. aligns the flag and the Text left
+                  alignLeft: false,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.phone,
+                onChanged: (value) {
+                  phone = value;
+                },
+                decoration: InputDecoration(
+                    fillColor: Colors.teal,
+                    focusColor: Colors.teal,
+                    hoverColor: Colors.teal,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: "Phone"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                onChanged: (value) {
+                  msg = value;
+                },
+                decoration: InputDecoration(
+                    fillColor: Colors.teal,
+                    focusColor: Colors.teal,
+                    hoverColor: Colors.teal,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: "Message"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.teal,
+                elevation: 5,
+                child: MaterialButton(
+                  child: const Text(
+                    'Send',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                      code = code.replaceAll("+", "");
+
+                      launch("https://wa.me/$code$phone?text=$msg");
+                      // var url = "https://wa.me/$code$phone?text=$msg";
+                      // if (await canLaunch(url)) {
+                      //   await launch(url);
+                      // } else {
+                      //   throw "Could not launch $url";
+                      // }
+                  },
+                  minWidth: 130,
+                  height: 40,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
